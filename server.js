302d8db79092ec387e1b03716971995c1cf37460
Alik-5 <aaleqsanyan048@gmail.com>
2026-03-19 21:03:12 +0400

    1231

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/leads',(req, res) => {
-    res.json([
-        {name: "Frank",age: 30},
-        {name: "John",age: 25}
-    ]);
+    res.json(
+        {
+            name: 'Frank Bowels',
+            age: 30
+        }
+    );
 });
 
 app.listen(PORT,() => {console.log(`Server is running on port ${PORT}`)});
